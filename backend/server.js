f9ae817c999c3e7062148bebf30de0f36cfa01ca
mint-made <thomaskupai@gmail.com>
2021-06-09 12:44:55 +0100

    mongoDB connected

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,33 @@
-const express = require('express');
+import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
 
-const products = require('./data/products');
+import connectDB from './config/db.js';
+import products from './data/products.js';
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('API is running');
+  res.send('API is running...');
 });
 
 app.get('/api/products', (req, res) => {
   res.json(products);
 });
 
-app.get('/api/products:id', (req, res) => {
+app.get('/api/products/:id', (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
 
-app.listen(5000, console.log('server running on 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(
+    `server running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold
+  )
+);
