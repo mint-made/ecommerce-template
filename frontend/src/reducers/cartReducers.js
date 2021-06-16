4663801b4c3fd0ebb3aee298f77d44201e0c6ece
mint-made <thomaskupai@gmail.com>
2021-06-16 22:41:33 +0100

    cart page + cart functionality actions/reducers implimented

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,10 +1,10 @@
-import { CART_ADD_ITEM } from '../constants/cartConstants';
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
     case CART_ADD_ITEM:
       const item = action.payload;
-      console.log({ ...state });
+
       const existItem = state.cartItems.find((x) => x.product === item.product);
 
       if (existItem) {
@@ -17,9 +17,15 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
       } else {
         return {
           ...state,
-          cartItems: [...state, item],
+          cartItems: [...state.cartItems, item],
         };
       }
+    case CART_REMOVE_ITEM:
+      console.log('remove from cart reducer');
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((x) => x.product !== action.payload),
+      };
     default:
       return state;
   }
