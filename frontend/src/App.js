ad209f376acadc88992d6e54b71710c411bd32b2
mint-made <thomaskupai@gmail.com>
2021-06-24 10:43:53 +0100

    payment and shipping screens/actions/reducers added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,8 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
 
 const App = () => {
   return (
@@ -22,6 +24,8 @@ const App = () => {
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/' component={HomeScreen} exact />
           <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/shipping' component={ShippingScreen} />
+          <Route path='/payment' component={PaymentScreen} />
         </Container>
       </main>
       <Footer />
