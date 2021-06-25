61127a2fa36d2c2cfd5409f989d498c67c157e53
mint-made <thomaskupai@gmail.com>
2021-06-25 16:19:41 +0100

    http endpoints added for getUserById and updateUser

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -71,13 +71,13 @@ export const logout = () => (dispatch) => {
     type: USER_DETAILS_RESET,
   });
   dispatch({
-    type: ORDER_LIST_MY_RESET,
-  });
-  dispatch({
     type: USER_LIST_RESET,
   });
   dispatch({
     type: USER_REGISTER_RESET,
+  });
+  dispatch({
+    type: ORDER_LIST_MY_RESET,
   });
 };
 
