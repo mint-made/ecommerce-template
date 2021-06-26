b1ab89b86db0416d7717139240c9abdd0b762a00
mint-made <thomaskupai@gmail.com>
2021-06-26 17:06:45 +0100

    updateUser endpoint logic bug fixed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -148,13 +148,11 @@ const getUserById = asyncHandler(async (req, res) => {
 // @access Private/Admin
 const updateUser = asyncHandler(async (req, res) => {
   const user = await User.findById(req.params.id);
-
+  console.log(req.body.isAdmin);
   if (user) {
     user.name = req.body.name || user.name;
     user.email = req.body.email || user.email;
-    if (req.body.isAdmin) {
-      user.isAdmin = req.body.isAdmin;
-    }
+    user.isAdmin = req.body.isAdmin;
 
     const updatedUser = await user.save();
 
