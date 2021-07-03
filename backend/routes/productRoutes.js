a7882b7c1841a7586442f154ce537a1490d8ada3
mint-made <thomaskupai@gmail.com>
2021-07-03 23:10:47 +0100

    created endpoints for create product and update product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,15 +3,18 @@ import {
   getProductById,
   getProducts,
   deleteProduct,
+  createProduct,
+  updateProduct,
 } from '../controllers/productController.js';
 import { protect, isAdmin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/').get(getProducts);
+router.route('/').get(getProducts).post(protect, isAdmin, createProduct);
 router
   .route('/:id')
   .get(getProductById)
-  .delete(protect, isAdmin, deleteProduct);
+  .delete(protect, isAdmin, deleteProduct)
+  .put(protect, isAdmin, updateProduct);
 
 export default router;
