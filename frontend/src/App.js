144adf335674bace913ac3c14335c9811f3333a5
mint-made <thomaskupai@gmail.com>
2021-07-09 22:07:22 +0100

    search endpoint added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -40,6 +40,7 @@ const App = () => {
           <Route path='/admin/userlist' component={UserListScreen} />
           <Route path='/admin/orderlist' component={OrderListScreen} />
           <Route path='/admin/productlist' component={ProductListScreen} />
+          <Route path='/search/:keyword' component={HomeScreen} />
           <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
