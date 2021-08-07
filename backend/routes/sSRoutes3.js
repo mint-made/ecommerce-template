497c5cd7205b5c75dd3a02e14a837f3672af662a
mint-made <thomaskupai@gmail.com>
2021-08-07 12:14:23 +0100

    File deleted from server after it is uploaded to s3

--- backend/routes/sSRoutes3.js
+++ backend/routes/sSRoutes3.js
@@ -4,9 +4,12 @@ import dotenv from 'dotenv';
 import multer from 'multer';
 import S3 from 'aws-sdk/clients/s3.js';
 import { triggerAsyncId } from 'async_hooks';
+import path from 'path';
+import util from 'util';
 
 dotenv.config();
 const router = express.Router();
+const unlinkFile = util.promisify(fs.unlink);
 
 const s3 = new S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -26,35 +29,35 @@ const uploadFileToS3 = (file) => {
   return s3.upload(uploadParams).promise();
 };
 
-const upload = multer({ dest: 'uploads/' });
+// Checks that uploaded images are either: .jpg/.jpeg/.png
+function checkFileType(file, cb) {
+  const filetypes = /jpg|jpeg|png/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb('Images only!');
+  }
+}
+
+const upload = multer({
+  dest: 'uploads/',
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
 
 router.post('/', upload.single('image'), async (req, res) => {
   try {
     const result = await uploadFileToS3(req.file);
-    console.log(result.Location);
+
+    // Deletes the file from the server after it has been uploaded to S3
+    await unlinkFile(req.file.path);
+
+    // Returns a public image URL for the successfully uploaded image
     res.send({ imagePath: result.Location });
-  } catch (error) {
-    res.status(400).json(error);
-  }
-});
-
-// downloads a file from s3
-const getFileStream = (fileKey) => {
-  const downloadParams = {
-    Key: fileKey,
-    Bucket: process.env.AWS_S3_BUCKET_NAME,
-  };
-
-  s3.getObject(downloadParams).createReadStream();
-};
-
-router.get('/:key', (req, res) => {
-  console.log('hit');
-  try {
-    const key = req.params.key;
-    const readStream = getFileStream(key);
-
-    readStream.pipe(res);
   } catch (error) {
     res.status(400).json(error);
   }
